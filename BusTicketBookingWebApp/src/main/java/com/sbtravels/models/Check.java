b1af37be8aaf46d1a33c690422b42e4e5fe7d86b
Souvik Biswas <you@example.com>
2023-07-03 00:49:44 +0530

    initial commit

---
+++ BusTicketBookingWebApp/src/main/java/com/sbtravels/models/Check.java
@@ -0,0 +1,17 @@
+package com.sbtravels.models;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Check {
+
+	public static void main(String[] args) {
+		
+		Logger log = LoggerFactory.getLogger(Check.class);
+		
+		log.warn("I am Souvik");
+
+	}
+
+}
